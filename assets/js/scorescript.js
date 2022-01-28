2ba57aa6fc5e5d55c5bd9bc1092480e15a3a9633
priscilla ma <priscillama621@gmail.com>
2022-01-27 20:55:41 -0500

    edited and added comments to all

--- assets/js/scorescript.js
+++ assets/js/scorescript.js
@@ -1,5 +1,9 @@
+// Area to display high scores
 var highscoreDisplay = document.querySelector("#highscore-display");
+
+// Values from local storage
 var initials = localStorage.getItem("initials");
 var score = localStorage.getItem("score");
 
+// Use text content to push grabbed data to display area
 highscoreDisplay.textContent = initials + " " + score;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,51 @@
-// when I click start button, timer begins and I am presented with question
-// when I answer question, then presented with another question
-// when I answer incorrectly, time is subtracted from the clock
-// when all questions r answered or time runs out, then the game is over
-// when the game is over, I can save my initials and score.
-
-//append to main
-
-// var mainPage = document.querySelector("#main-page");
-
-
-//Timer
+// Assignments
+// Timer
 var timer = document.querySelector("#timer");
-
+// Seconds remaining
 var countdown = 60;
-
+// Keeping track of score - added to and deducted from this variable
 var score = 0;
-//thinking i'll do var SCORE = 0; and then some math shit to triple the score, but each time click correct or incorrect the SCORE = 0 will be added to or decreased from.
-
-
+// Start button
 var startQuizButton = document.querySelector("#start-quiz-button");
-var questionContainer = document.querySelector("#question-container"); //question 1
-var questionLine = document.querySelector("#question"); //question 1
-// var answerButtons = document.querySelector("#answer-buttons");
-var answerOne = document.querySelector("#answer-1"); //question 1
-var answerTwo = document.querySelector("#answer-2"); //question 1
-var answerThree = document.querySelector("#answer-3"); //question 1
 
-//question 2
+// Question 1 container
+var questionContainer = document.querySelector("#question-container");
+var questionLine = document.querySelector("#question");
+var answerOne = document.querySelector("#answer-1");
+var answerTwo = document.querySelector("#answer-2");
+var answerThree = document.querySelector("#answer-3");
+
+// Question 2 container
 var questionTwoContainer = document.querySelector("#question2-container");
 var questionTwoLine = document.querySelector("#question2");
 var answerTwoPointOne = document.querySelector("#answer-2-1");
 var answerTwoPointTwo = document.querySelector("#answer-2-2");
 var answerTwoPointThree = document.querySelector("#answer-2-3");
 
-//Exit page
-var gameOver = document.querySelector("#game-over");
-var submitButton = document.querySelector("#submit-btn");
-
-
-// var questions = [
-//     // {
-//     //     question: "what is my favourite fruit?",
-//     //     answers: [
-//     //         {text: "peach", correct: true},
-//     //         {text: "grape", correct: false},                            //index[0]
-//     //         {text: "apple", correct: false}
-//     //     ]
-//     // },
-//     // {
-//     //     question: "what colour are you?",
-//     //     answers: [
-//     //         {text: "blue", correct: false},
-//     //         {text: "purple", correct: true},                            //index[1]
-//     //         {text: "yellow", correct: false}
-//     //     ]
-//     // }
-// ]
-
+// The actual questions and answers
 var questionOne = {
     question: "how do you link js to your html?",
     answers: ["link", "script", "href"],
 };
-
 var questionTwo = {
     question: "what could you use to select an element?",
     answers: ["querySelector", "getElementById", "both!"],
 }
 
-// var questionsGoHere = document.querySelector(".container");
+//Exit page
+var gameOver = document.querySelector("#game-over");
+var submitButton = document.querySelector("#submit-btn");
 
-// After start quiz button is clicked and the quiz has started, the start button will hide.
+//Functions
+// After start quiz button is clicked and the quiz has started, the start button will hide
 function startQuiz(){
     startQuizButton.classList.add("hide");
     questionContainer.classList.remove("hide");
     startTimer();
     firstQuestion();
-    
-    // var index = 0;
-    // var questions = [
-    //     {
-    //         question: "what is my favourite fruit?",
-    //         correctAnswer: "peach",
-    //         answerTwo: "grape",
-    //         answerThree: "apple"
-    //     },
-    //     {
-    //         question: "what colour are you?",
-    //         answerOne: "blue",
-    //         correctAnswer: "purple",
-    //         answerThree: "yellow"
-    //     }
-    // ]
-
 }
 
-// Timer function after it is called by clicking start quiz, starts to count down from 60 secs. When time runs out, goes to game over page.
+// Timer function after it is called by clicking start quiz, starts to count down from 60 secs. When time runs out, goes to game over page
 function startTimer(){
     var timerInterval = setInterval(function (){
         countdown--;
@@ -106,18 +57,22 @@ function startTimer(){
     }, 1000);
 }
 
+// First question
 function firstQuestion(){
+    // Pushes the actual question to the question line
     questionLine.textContent = questionOne.question;
+    // Pushes the answers to the answer buttons
     answerOne.textContent = questionOne.answers[0];
     answerTwo.textContent = questionOne.answers[1];
     answerThree.textContent = questionOne.answers[2];
-
+    // Answer is incorrect, alert, minus time, negative score, calls second question function
     answerOne.addEventListener("click", function(){
         alert("incorrect :(");
         countdown -= 5;
         score -= 1 * 52000;
         secondQuestion();
     });
+    // Answer is correct, alert, adds time, adds score, calls second question function
     answerTwo.addEventListener("click", function(){
         alert("correct!");
         countdown += 5;
@@ -130,20 +85,19 @@ function firstQuestion(){
         score -= 1 * 52000;
         secondQuestion();
     });
-
-    // answerButtons.textContent = questions[0].answers;
-    // var index = 0;
-    // showQuestion()
 }
 
+// Second question
 function secondQuestion(){
+    // Hide first question
     questionContainer.classList.add("hide");
+    // Make question 2 appear
     questionTwoContainer.classList.remove("hide");
     questionTwoLine.textContent = questionTwo.question;
     answerTwoPointOne.textContent = questionTwo.answers[0];
     answerTwoPointTwo.textContent = questionTwo.answers[1];
     answerTwoPointThree.textContent = questionTwo.answers[2];
-
+    // Incorrect answer, alert, deducts time, deducts score, calls exit page function
     answerTwoPointOne.addEventListener("click", function(){
         alert("incorrect :(");
         countdown -= 5;
@@ -164,18 +118,24 @@ function secondQuestion(){
     });
 }
 
+// Exit page / game over page
 function Exit(){
+    // Hides timer
     timer.classList.add("hide");
+    // Hides question 1
     questionContainer.classList.add("hide");
+    // Hides question 2
     questionTwoContainer.classList.add("hide");
+    // Makes exit/game over page appear
     gameOver.classList.remove("hide");
+    // Pushes final score to user score line
     document.querySelector("#user-score").textContent = "your score is: " + score;
 }
 
-
+// Start Button
 startQuizButton.addEventListener("click", startQuiz)
-    // startQuizButton.hidden = true;
 
+// Submit initials button, sets user's initials and score values to local storage
 submitButton.addEventListener("click", function(event){
     event.preventDefault();
     var initials = document.querySelector("#initials").value;
